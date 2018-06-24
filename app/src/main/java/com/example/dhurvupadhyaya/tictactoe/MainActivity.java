67ed268da8d6c6a78c1c11e1ed860f9d99f8983a
DhruvUpadhyaya <40164639+DhruvUpadhyaya@users.noreply.github.com>
2018-06-24 16:36:22 +0530

    Players turn implemented

--- app/src/main/java/com/example/dhurvupadhyaya/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/dhurvupadhyaya/tictactoe/MainActivity.java
@@ -2,9 +2,33 @@ package com.example.dhurvupadhyaya.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  int myActivePlayer = 0;
+    public void onTapped(View view){
+
+        ImageView myTapped = (ImageView) view;
+        if (myActivePlayer == 0)
+        {
+            myTapped.setImageResource(R.drawable.cross);
+            myTapped.animate().rotation(360).setDuration(1000);
+            myActivePlayer = 1;
+
+        }else
+        {
+            myTapped.setImageResource(R.drawable.circle);
+            myTapped.animate().rotation(360).setDuration(1000);
+            myActivePlayer = 0;
+        }
+
+
+
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
